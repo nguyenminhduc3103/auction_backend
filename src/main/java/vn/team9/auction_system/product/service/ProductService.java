622fb210b382aac08602283397328cf1993521fd
Dukzf1v <ducminh5404@gmail.com>
2025-11-11 18:02:46 +0700

    .

--- src/main/java/vn/team9/auction_system/product/service/ProductService.java
+++ src/main/java/vn/team9/auction_system/product/service/ProductService.java
@@ -117,6 +117,7 @@ public class ProductService implements IProductService {
 	@Transactional(readOnly = true)
 	public List<ProductResponse> getProductsBySeller(@NonNull Long sellerId) {
 		return productRepository.findBySeller_UserIdAndIsDeletedFalse(sellerId).stream()
+				.filter(p -> "approved".equalsIgnoreCase(p.getStatus()))
 				.map(productMapper::toResponse)
 				.collect(Collectors.toList());
 	}
