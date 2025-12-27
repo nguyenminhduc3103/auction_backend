c2775842a8e13f064f66880631238bee24a809fc
Dukzf1v <ducminh5404@gmail.com>
2025-12-27 11:42:52 +0700

    .

---
+++ target/generated-sources/annotations/vn/team9/auction_system/feedback/mapper/FeedbackMapperImpl.java
@@ -0,0 +1,199 @@
+package vn.team9.auction_system.feedback.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+import vn.team9.auction_system.common.dto.admin.UserWarningLogRequest;
+import vn.team9.auction_system.common.dto.admin.UserWarningLogResponse;
+import vn.team9.auction_system.common.dto.feedback.FeedbackRequest;
+import vn.team9.auction_system.common.dto.feedback.FeedbackResponse;
+import vn.team9.auction_system.common.dto.notification.NotificationRequest;
+import vn.team9.auction_system.common.dto.notification.NotificationResponse;
+import vn.team9.auction_system.feedback.model.Feedback;
+import vn.team9.auction_system.feedback.model.Notification;
+import vn.team9.auction_system.feedback.model.UserWarningLog;
+import vn.team9.auction_system.transaction.model.TransactionAfterAuction;
+import vn.team9.auction_system.user.model.User;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-12-27T11:40:36+0700",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.44.0.v20251118-1623, environment: Java 21.0.9 (Eclipse Adoptium)"
+)
+@Component
+public class FeedbackMapperImpl implements FeedbackMapper {
+
+    @Override
+    public Feedback toEntity(FeedbackRequest request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        Feedback feedback = new Feedback();
+
+        feedback.setComment( request.getComment() );
+        feedback.setRating( request.getRating() );
+
+        return feedback;
+    }
+
+    @Override
+    public FeedbackResponse toResponse(Feedback entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        FeedbackResponse feedbackResponse = new FeedbackResponse();
+
+        feedbackResponse.setUserId( entityUserUserId( entity ) );
+        feedbackResponse.setUsername( entityUserUsername( entity ) );
+        feedbackResponse.setComment( entity.getComment() );
+        feedbackResponse.setCreatedAt( entity.getCreatedAt() );
+        feedbackResponse.setFeedbackId( entity.getFeedbackId() );
+        if ( entity.getRating() != null ) {
+            feedbackResponse.setRating( entity.getRating() );
+        }
+
+        return feedbackResponse;
+    }
+
+    @Override
+    public Notification toEntity(NotificationRequest request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        Notification notification = new Notification();
+
+        notification.setMessage( request.getMessage() );
+        notification.setType( request.getType() );
+
+        return notification;
+    }
+
+    @Override
+    public NotificationResponse toResponse(Notification entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        NotificationResponse notificationResponse = new NotificationResponse();
+
+        notificationResponse.setUserId( entityUserUserId1( entity ) );
+        notificationResponse.setCreatedAt( entity.getCreatedAt() );
+        notificationResponse.setMessage( entity.getMessage() );
+        notificationResponse.setType( entity.getType() );
+
+        return notificationResponse;
+    }
+
+    @Override
+    public UserWarningLog toEntity(UserWarningLogRequest request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        UserWarningLog userWarningLog = new UserWarningLog();
+
+        userWarningLog.setDescription( request.getDescription() );
+        userWarningLog.setStatus( request.getStatus() );
+        userWarningLog.setType( request.getType() );
+        userWarningLog.setViolationCount( request.getViolationCount() );
+
+        return userWarningLog;
+    }
+
+    @Override
+    public UserWarningLogResponse toResponse(UserWarningLog entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        UserWarningLogResponse userWarningLogResponse = new UserWarningLogResponse();
+
+        userWarningLogResponse.setUserId( entityUserUserId2( entity ) );
+        userWarningLogResponse.setTransactionId( entityTransactionTransactionId( entity ) );
+        userWarningLogResponse.setCreatedAt( entity.getCreatedAt() );
+        userWarningLogResponse.setDescription( entity.getDescription() );
+        userWarningLogResponse.setLogId( entity.getLogId() );
+        userWarningLogResponse.setStatus( entity.getStatus() );
+        userWarningLogResponse.setType( entity.getType() );
+        userWarningLogResponse.setViolationCount( entity.getViolationCount() );
+
+        return userWarningLogResponse;
+    }
+
+    private Long entityUserUserId(Feedback feedback) {
+        if ( feedback == null ) {
+            return null;
+        }
+        User user = feedback.getUser();
+        if ( user == null ) {
+            return null;
+        }
+        Long userId = user.getUserId();
+        if ( userId == null ) {
+            return null;
+        }
+        return userId;
+    }
+
+    private String entityUserUsername(Feedback feedback) {
+        if ( feedback == null ) {
+            return null;
+        }
+        User user = feedback.getUser();
+        if ( user == null ) {
+            return null;
+        }
+        String username = user.getUsername();
+        if ( username == null ) {
+            return null;
+        }
+        return username;
+    }
+
+    private Long entityUserUserId1(Notification notification) {
+        if ( notification == null ) {
+            return null;
+        }
+        User user = notification.getUser();
+        if ( user == null ) {
+            return null;
+        }
+        Long userId = user.getUserId();
+        if ( userId == null ) {
+            return null;
+        }
+        return userId;
+    }
+
+    private Long entityUserUserId2(UserWarningLog userWarningLog) {
+        if ( userWarningLog == null ) {
+            return null;
+        }
+        User user = userWarningLog.getUser();
+        if ( user == null ) {
+            return null;
+        }
+        Long userId = user.getUserId();
+        if ( userId == null ) {
+            return null;
+        }
+        return userId;
+    }
+
+    private Long entityTransactionTransactionId(UserWarningLog userWarningLog) {
+        if ( userWarningLog == null ) {
+            return null;
+        }
+        TransactionAfterAuction transaction = userWarningLog.getTransaction();
+        if ( transaction == null ) {
+            return null;
+        }
+        Long transactionId = transaction.getTransactionId();
+        if ( transactionId == null ) {
+            return null;
+        }
+        return transactionId;
+    }
+}
---
+++ target/generated-sources/annotations/vn/team9/auction_system/user_report/mapper/UserReportMapperImpl.java
@@ -0,0 +1,44 @@
+package vn.team9.auction_system.user_report.mapper;
+
+import javax.annotation.processing.Generated;
+import vn.team9.auction_system.common.dto.user_report.UserReportRequest;
+import vn.team9.auction_system.common.dto.user_report.UserReportResponse;
+import vn.team9.auction_system.user_report.model.UserReport;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-12-27T11:40:35+0700",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.44.0.v20251118-1623, environment: Java 21.0.9 (Eclipse Adoptium)"
+)
+public class UserReportMapperImpl implements UserReportMapper {
+
+    @Override
+    public UserReport toEntity(UserReportRequest request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        UserReport userReport = new UserReport();
+
+        userReport.setContent( request.getContent() );
+        userReport.setUserId( request.getUserId() );
+
+        return userReport;
+    }
+
+    @Override
+    public UserReportResponse toResponse(UserReport entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        UserReportResponse userReportResponse = new UserReportResponse();
+
+        userReportResponse.setContent( entity.getContent() );
+        userReportResponse.setCreatedAt( entity.getCreatedAt() );
+        userReportResponse.setId( entity.getId() );
+        userReportResponse.setUserId( entity.getUserId() );
+
+        return userReportResponse;
+    }
+}
