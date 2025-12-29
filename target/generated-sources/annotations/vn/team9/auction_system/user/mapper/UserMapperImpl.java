b9128621f5b8905ec319f9aa52a48164bd779fe6
Ducminh1905 <160600107+Ducminh1905@users.noreply.github.com>
2025-12-29 18:10:56 +0700

    1

--- target/generated-sources/annotations/vn/team9/auction_system/user/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/vn/team9/auction_system/user/mapper/UserMapperImpl.java
@@ -8,7 +8,7 @@ import vn.team9.auction_system.user.model.User;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-12-29T13:26:42+0700",
+    date = "2025-12-29T18:07:05+0700",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.44.0.v20251118-1623, environment: Java 21.0.9 (Eclipse Adoptium)"
 )
 @Component
